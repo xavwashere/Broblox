d5cfa9fa05cbdb4747de03d847e0e5b5423275de
Exyrus <97399129+Exyrus@users.noreply.github.com>
2022-11-07 17:03:35 +0000

    Testing

---
+++ DOORS/Crucifix.lua
@@ -0,0 +1,12 @@
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+local Window = Rayfield:CreateWindow({
+	Name = "DOORS Crucifix Giver",
+	LoadingTitle = "brb: sniffing your balls",
+	LoadingSubtitle = "ok im nearly done",
+	ConfigurationSaving = {
+		Enabled = true,
+		FolderName = "DOORS",
+		FileName = "Crucifix Giver"
+	},
+	KeySystem = false, -- Set this to true to use our key system
+})
